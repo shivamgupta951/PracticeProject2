19117073fd0328f55f6de2a3233cb7c5e5300984
Shivam Gupta <kapilmohangupta20@gmail.com>
2026-03-22 20:23:55 +0530

    created middle-ware for page access!

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,6 +3,7 @@ import { signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
+import { FaPencilAlt } from "react-icons/fa";
 
 function page() {
   const { data } = useSession();
@@ -32,7 +33,10 @@ function page() {
           </div>
         )}
         <div>Welcome, {data?.user.name}!</div>
-        <div className="absolute border top-3 rounded-md text-[60%] p-2 right-0 border-r-0 bg-linear-to-tl from-purple-950 to-orange-800 cursor-pointer transition-all transform duration-300 ease-in-out animate-pulse tracking-wider px-4 hover:scale-105" onClick={handleSignOut}>
+        <div className="absolute border top-0 rounded-bl-2xl text-[60%] p-2 right-0 border-r-0 bg-linear-to-br from-red-700 to-orange-800 via-black cursor-pointer transition-all rounded-se-2xl transform duration-300 ease-in-out animate-pulse tracking-wider px-4 justify-center items-center flex hover:scale-105" onClick={()=> router.push("/edit")}>
+          Edit Profile <FaPencilAlt className="mx-1 text-[80%]"/>
+        </div>
+        <div className="absolute border top-0 rounded-tl-2xl rounded-br-2xl text-[60%] p-2 left-0 border-r-0 bg-linear-to-tl from-purple-950 to-orange-800 cursor-pointer transition-all transform duration-300 ease-in-out animate-pulse tracking-wider px-4 hover:scale-105" onClick={handleSignOut}>
           Sign out
         </div>
       </div>
