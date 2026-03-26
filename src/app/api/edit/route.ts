73a47faf28a3c799195a91b300052615efb46ef1
Shivam Gupta <kapilmohangupta20@gmail.com>
2026-03-26 17:04:39 +0530

    profile page and notes section created with api!

---
+++ src/app/api/edit/route.ts
@@ -0,0 +1,54 @@
+import uploadOnCloudinary from "@/lib/cloudinary";
+import connectDb from "@/lib/db";
+import User from "@/model/user.model";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  try {
+    await connectDb();
+    const session = await getServerSession();
+    if (!session || session.user.email || session.user.id) {
+      return NextResponse.json(
+        {
+          message: "user does not have session.",
+        },
+        { status: 400 },
+      );
+    }
+
+    const formData = await req.formData();
+    const name = formData.get("name") as string;
+    const file = formData.get("file") as Blob | null;
+    let imageUrl = session.user.image ?? null;
+    if(file)
+    {
+        imageUrl = await uploadOnCloudinary(file);
+    }
+    const user = await User.findByIdAndUpdate(session.user.id,{
+        name,image:imageUrl
+    },{new:true});
+
+    if(!user)
+    {
+        return NextResponse.json(
+        {
+          message: "user not found.",
+        },
+        { status: 400 },
+      );
+    }
+
+    return NextResponse.json(
+        user,
+        { status: 200 },
+      );
+  } catch (error) {
+    return NextResponse.json(
+        {
+          message: `edit error ${error}`,
+        },
+        { status: 500 },
+      );
+  }
+}
--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
 
 function page() {
   return (
-    <div className='text-white min-h-screen bg-linear-to-br from-red-950 via-black to-red-900 flex justify-center items-center text-2xl'>
-        TextNotes Page!
+    <div className="text-white min-h-screen bg-linear-to-br from-red-950 via-black to-red-900 flex justify-around items-center text-2xl">
+      <div className="w-[20%] border min-h-screen flex justify-start items-center rounded-4xl bg-linear-to-b from-ourple-400 via-blue-950 to-[#0b0525] flex-col">
+        <div className="mt-7 border-b-4 flex justify-center items-center w-full pb-4 border-yellow-600 font-bold">
+          Notes Bar
+        </div>
+        <div className="py-20 relative space-y-12 tracking-tighter text-[80%] border-b-3 border-dashed w-full px-[5%] min-h-[500px]">
+          <div className="absolute -bottom-10 left-[22%] bg-linear-to-r from-red-600 via-yellow-400 to-green-700 bg-clip-text text-transparent text-[90%] tracking-widest">max-limit 5 notes!</div>
+          <div className="border-2 p-2 px-8 rounded-b-3xl flex justify-center items-center bg-linear-to-tl from-red-950 via-orange-900 to-gray-700 transition-all transform duration-700 ease-in-out hover:scale-105 hover:rotate-2 cursor-pointer shadow-2xl shadow-orange-950">Engneering Mathematics</div>
+          <div className="border-2 p-2 px-8 rounded-b-3xl flex justify-center items-center bg-linear-to-tl from-red-950 via-orange-900 to-gray-700 transition-all transform duration-700 ease-in-out hover:scale-105 hover:rotate-2 cursor-pointer shadow-2xl shadow-orange-950">Physics</div>
+          <div className="border-2 p-2 px-8 rounded-b-3xl flex justify-center items-center bg-linear-to-tl from-red-950 via-orange-900 to-gray-700 transition-all transform duration-700 ease-in-out hover:scale-105 hover:rotate-2 cursor-pointer shadow-2xl shadow-orange-950">English</div>
+        </div>
+      </div>
+      <div className="w-[78%] p-6 border min-h-screen rounded-l-4xl flex flex-wrap justify-start">
+        <div className="h-60 w-60 flex justify-center items-center rounded-bl-4xl border mx-4 bg-yellow-200 text-black">Note Box</div>
+        <div className="h-60 w-60 flex justify-center items-center rounded-bl-4xl border mx-4 bg-yellow-200 text-black">Note Box</div>
+        <div className="h-60 w-60 flex justify-center items-center rounded-bl-4xl border mx-4 bg-yellow-200 text-black">Note Box</div>
+        <div className="h-60 w-60 flex justify-center items-center rounded-bl-4xl border mx-4 bg-yellow-200 text-black">Note Box</div>
+        <div className="h-60 w-60 flex justify-center items-center rounded-bl-4xl border mx-4 bg-yellow-200 text-black">Note Box</div>
+        <div className="h-60 w-60 flex justify-center items-center rounded-bl-4xl border mx-4 bg-yellow-200 text-black">Note Box</div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default page
+export default page;
